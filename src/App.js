3bf781191fd69aadcf47f4b2ccff6828a05da592
Yusa Islam <yusaislam48@gmail.com>
2021-03-03 02:27:19 +0600

    use axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import News from "./Components/News/News";
+import axios from 'axios';
 
 function App() {
   const [articles, setArticles] = useState([]);
-  useEffect(() =>{
-    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=81fdb40ab756421e9d4e34a2aac4d273')
-    .then(res => res.json())
-    .then(data => {
-      setArticles(data.articles);
-    })
-  },[])
+
+  // useEffect(() =>{
+  //   fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=81fdb40ab756421e9d4e34a2aac4d273')
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     setArticles(data.articles);
+  //   })
+  // },[])
+
+  useEffect( () => {
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=81fdb40ab756421e9d4e34a2aac4d273';
+    axios(url)
+    .then(data => setArticles(data.data.articles))
+  }, [])
 
   return (
     <div>
